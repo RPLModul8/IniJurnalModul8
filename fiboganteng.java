6acd294add204be7718757d03246d8f78c265497
Abdillah Israqi Zihni <abdillahiz@users.noreply.github.com>
2014-04-21 18:07:40 +0700

    Update fiboganteng.java

--- fiboganteng.java
+++ fiboganteng.java
@@ -3,13 +3,19 @@ public class fiboganteng{
         Scanner input = new Scanner(System.in);
         System.out.print("Masukan angka : ");
         int angka = input.nextInt();;
+        int jumlah[] = new int[angka];
         int a=0;
         int b=1;
 		
 	for (int i=0;i<angka;i++) {
             a=a+b;
-            b=a-b; 
-            System.out.print(a+" ");
-    }
+            b=a-b;
+            jumlah[i]=a;
+        }
+        
+        for (int j=angka; j>=0; j--) {
+            int d=jumlah[j];
+            System.out.print(d+" ");
+        }
     }
 }
